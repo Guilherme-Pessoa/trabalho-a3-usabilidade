393e9dcfe899547c589dbaad995fc78a79ebb6a4
Renan Abreu <renansab2011@gmail.com>
2022-12-09 20:20:05 -0300

    done-atts-in-front-and-readme

---
+++ README.md
@@ -0,0 +1,117 @@
+# (A3) Usabilidade, desenvolvimento web, mobile e jogos: 
+
+## Apresentação
+Esse projeto foi realizado pelos alunos da turma de Usabilidade, desenvolvimento web, mobile e jogos do turno noturno da UNIFACS (Nomes e RA's dos alunos no arquivo PDF)
+
+## Descrição
+Este projeto possui o propósito de realizar um sistema escolar onde você possar criar, ler, atualizar e deletar registros.
+
+## Tecnologias Utilizadas
+* React;
+* Node.js;
+* Express;
+* MySQL;
+* Vite (Ferramenta de construção ao qual utilizamos para criar o projeto com o React);
+* Tailwind (Framework de estilização).
+
+## Pré-Requisitos
+* Possuir instalado em sua máquina uma IDE, como o VsCode;
+* Clonar este repositório em sua máquina:
+
+HTTPS:
+```
+git clone https://github.com/the-abreu/A3-SD-Client-And-Server.git
+```
+
+## Bibliotecas e outras dependências
+Após clonar este repositório e o abrir em sua IDE de preferência, será necessário que digite em seu
+terminal os seguintes comandos:
+
+* Para abrir a pasta do servidor:
+
+SERVER:
+```
+cd server
+```
+
+* Para instalar as dependências esperadas dentro da pasta:
+
+DEPENDÊNCIAS:
+```
+npm install
+```
+
+* Para inicializar o servidor:
+
+INICIALIZAR O SERVIDOR:
+```
+npm start
+```
+
+* Agora inicialize um segundo terminal, que por padrão virá na raiz do projeto, e instale as dependências esperadas:
+
+DEPENDÊNCIAS:
+```
+npm install
+```
+
+* Importe a biblioteca Axios:
+
+AXIOS:
+```
+npm install axios
+```
+
+* Para inicializar o projeto:
+
+INICIALIZAR O projeto:
+```
+npm run dev
+```
+
+## Como manusear o sistema
+Estando na página inicial, você irá se deparar com as primeiras funcionalidades disponíveis, que são botões que te conduzem as rotas desejadas.
+
+## Introdução as funcionalidades do sistema
+Estando na página inicial, você irá se deparar com as primeiras funcionalidades disponíveis, que são botões que te conduzem as rotas desejadas. Abordaremos um pouco a cerca das páginas para proporcionar uma contextualização:
+
+![menu](https://user-images.githubusercontent.com/96211934/204113855-5968d13f-75f6-49b3-a6f6-22cc49093337.png)
+
+
+<details open>
+  <summary>
+    <b>Consultar Aluno</b>
+  </summary>
+  
+  ![vendedor](https://user-images.githubusercontent.com/96211934/204113805-e90e2ad1-1b3d-4149-a30b-7739a51ba992.png)
+
+- Funcionalidade: Nesta tela você poderá consultar os alunos matriculados no sistema, poderá editar seus registros, os deletar, ou cadastrar novos alunos.
+
+- FOTO
+</details>
+
+<details open>
+  <summary>
+    <b>Consultar Turmas</b>
+  </summary>
+  
+  ![gerente](https://user-images.githubusercontent.com/96211934/204113786-c907190d-2f69-42da-96ae-5f277d099f1e.png)
+
+
+- Funcionalidade: Nesta tela você poderá consultar as turmas criadas no sistema, poderá editar seus registros, os deletar, ou cadastrar novos turmas.
+
+- FOTO
+</details>
+
+<details open>
+  <summary>
+    <b>Consultar Turmas</b>
+  </summary>
+  
+  ![gerente](https://user-images.githubusercontent.com/96211934/204113786-c907190d-2f69-42da-96ae-5f277d099f1e.png)
+
+
+- Funcionalidade: Nesta tela você poderá consultar os boletins dos alunos no sistema, poderá editar seus registros, os deletar, ou lançar novas notas.
+
+- FOTO
+</details>
--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -1,30 +1,76 @@
+import { Link } from "react-router-dom";
+
 export default function AppLayout(props) {
     return (
-        <div className="relative min-h-screen bg-gray-200 pb-8">
+        <div className="">
             <title>
             {props.titulo}
 
             </title>
             
-            <header className="p-2 mb-2 bg-sky-600 h-16">
-                <img src="imagens/download.jpg" alt="Img nao suportada" width={66} className="" />
-                <h1 className="cabecalho">Escola</h1>
+<nav class="bg-white border-gray-200 dark:bg-sky-600">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a  class="flex items-center">
+            <img src="imagens/school.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Escola A3</span>
+        </a>
+        <div class="flex items-center">
+            <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">CONTATO: (71) 99999-9999</a>
+        </div>
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-sky-900">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                <li>
+                    <a href="/" class="text-gray-900 dark:text-white hover:underline">INICIO</a>
+                </li>
+                <li>
+                    <a href="/ConsultaAluno" class="text-gray-900 dark:text-white hover:underline">CONSULTAR ALUNOS</a>
+                </li>
+                <li>
+                    <a href="/ConsultaTurma" class="text-gray-900 dark:text-white hover:underline">CONSULTAR TURMAS</a>
+                </li>
+                <li>
+                    <a href="/Boletim" class="text-gray-900 dark:text-white hover:underline">BOLETIM</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-            </header>
-
-            <main className="container mx-auto bg-gray-500 p-2 rounded bg-white flex justify-center ">
+            <main className="container mx-auto p-2 flex justify-center ">
                 {props.children}
             </main>
 
-            <footer className="absolute bottom-0 w-full bg-sky-600 text-center h-12">
-                <span className="text-sm text-white">
-                    Endereço da escola
-                </span>
-                <br />
-                <span className="text-sm text-white">
-                    CNPJ
-                </span>
-            </footer>
+<footer class="bg-green-500 text-center lg:text-left">
+  <div class="container p-6 text-black">
+    <div class="grid lg:grid-cols-2 gap-4">
+    <div class="mb-6 md:mb-0">
+        <span class="font-medium mb-2 uppercase">CNPJ: </span>
+
+        <p class="mb-4">
+            07.960.956/0001-84
+        </p>
+      </div>
+
+      <div class="mb-6 md:mb-0">
+        <span class="font-medium mb-2 uppercase">ENDEREÇO: </span>
+
+        <p class="mb-4">
+          Rua N° 12 da Picket Post Close, em Martins Heron, na cidade de Bracknell.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="text-center text-black-700 p-4 bg-green-900">
+    <span class="text-white">
+        © 2022 Copyright: A3 School por Ariel Soares, Cleison Rocha, Guilherme Pessoa e Renan Abreu
+    </span>
+  </div>
+</footer>
 
         </div>
     )
